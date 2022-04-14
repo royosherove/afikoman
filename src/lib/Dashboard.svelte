acd544561e9f88b9b8618392d69dbafcc6ac91b4
Theeylon <aviveylon2@gmail.com>
2022-04-14 17:19:53 +0300

    Update Dashboard.svelte

--- src/lib/Dashboard.svelte
+++ src/lib/Dashboard.svelte
@@ -15,7 +15,7 @@
   <div
     class="bg-gray-300 text-black m-2 sm:w-1/3 p-4 font-bold sm:m-auto rounded-lg text-sm flex flex-col"
   >
-    <div>כרטיסים שנקנו: {tickets}</div>
+    <div>מספר מחפשי אפיקומן: {tickets}</div>
     <!-- svelte-ignore a11y-label-has-associated-control -->
 {#if hasWinner}
     <div class="flex flex-col mt-4 ">הארנק שניצח<div alt='winner'>{winner}</div></div>
