f8deafb670d8a8924876bddd2003d4b27b8124cf
royo <royo>
2022-04-14 19:12:41 +0300

    info text format

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
   import ConnectButton from "$lib/ConnectButton.svelte";
   import Dashboard from "$lib/Dashboard.svelte";
-import LinksBoard from "$lib/LinksBoard.svelte";
+  import Info from "$lib/Info.svelte";
+  import LinksBoard from "$lib/LinksBoard.svelte";
   import MintButton from "$lib/MintButton.svelte";
   import { globalState } from "../stores";
 </script>
@@ -10,10 +11,8 @@ import LinksBoard from "$lib/LinksBoard.svelte";
   <ConnectButton />
   <h1 class="text-center text-white p-12 mt-2 text-6xl">האפיקומן</h1>
   <h1 class="text-center text-white text-3xl">של קהילת ווב3</h1>
-   <h1 class="text-center text-white text-2xl">אז איך זה עובד?</h1>
-  <h1 class="text-center text-white text-1xl">יצרנו חוזה חכם והטמנו בתוכו ״אפיקומן״<br />כל כתובת יכולה ״לחפש״ את האפיקומן פעם אחת בחוזה<br />בתום סדר הפסח החוזה חכם יגיד לנו מי מהמחפשים ניצח ומצא את האפיקומן<br />כל המשתתפים יזכו בניפטי השתתפות. <br />אבל רק אחד יקבל את חבילת הפרס - הזכות לבחור נושא / פרוייקט מעניין, וחברי הקהילה וצוות המחקר של קוליידר יבצעו מחקר ויפרסמו את הסקירה בקהילה בספייס הבא בטוויטר
-</h1>
-  {#if ( $globalState.ownedToken === undefined || $globalState.ownedToken === 0)}
+  <Info />
+  {#if $globalState.ownedToken === undefined || $globalState.ownedToken === 0}
     <MintButton />
   {:else}
     <div class="text-center">
@@ -24,12 +23,17 @@ import LinksBoard from "$lib/LinksBoard.svelte";
     </div>
   {/if}
   <Dashboard />
-  <LinksBoard/>
-  <div class="fixed bottom-0 p-2 m-auto text-center text-gray-400 text-sm italic flex space-x-6">
-      <div class="">
-        Credits: Yotam Bar-On, ChagitR, Royo, Eylon
-      </div>
-      <a href='https://github.com/royosherove/afikoman' class="text-blue-400 ">github</a>
-      <a href='https://polygonscan.com/address/{$globalState.contractAddress}#code' class="text-blue-400 ">contract</a>
+  <LinksBoard />
+  <div
+    class="fixed bottom-0 p-2 m-auto text-center text-gray-400 text-sm italic flex space-x-6"
+  >
+    <div class="">Credits: Yotam Bar-On, ChagitR, Royo, Eylon</div>
+    <a href="https://github.com/royosherove/afikoman" class="text-blue-400 "
+      >github</a
+    >
+    <a
+      href="https://polygonscan.com/address/{$globalState.contractAddress}#code"
+      class="text-blue-400 ">contract</a
+    >
   </div>
 </body>
