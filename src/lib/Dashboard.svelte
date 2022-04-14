3a253abb17ce527e3cd4aab5e868f2a1aab4fec5
Theeylon <aviveylon2@gmail.com>
2022-04-14 14:22:16 +0300

    Update Dashboard.svelte

--- src/lib/Dashboard.svelte
+++ src/lib/Dashboard.svelte
@@ -13,8 +13,8 @@
   <div
     class="bg-gray-300 text-black m-2 sm:w-1/3 p-4 font-bold sm:m-auto rounded-lg text-sm flex flex-col"
   >
-    <div>מספר מחפשי אפיקומן כרגע: {tickets}</div>
+    <div>מספר מחפשי/מחפשות אפיקומן כרגע: {tickets}</div>
     <!-- svelte-ignore a11y-label-has-associated-control -->
-    <div class="flex flex-col">מחפש אפיקומן מנצח<div alt='winner'>{winner}</div></div>
+    <div class="flex flex-col">תמחפש.ת אפיקומן מנצח.ת<div alt='winner'>{winner}</div></div>
   </div>
 {/if}
