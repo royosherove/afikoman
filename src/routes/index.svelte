3034e1c4fa92caa5636e2f869e0ae410bec84874
Roy <roy@osherove.com>
2022-04-14 12:04:37 +0300

    Credits

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -5,17 +5,22 @@
   import { globalState } from "../stores";
 </script>
 
-<body class="bg-black h-screen text-white text-center">
+<body class="bg-black text-white text-center flex flex-col">
   <ConnectButton />
-  <h1 class="text-center text-white p-12 text-6xl">האפיקומן</h1>
+  <h1 class="text-center text-white p-12 mt-2 text-6xl">האפיקומן</h1>
   <h1 class="text-center text-white text-2xl">של קהילת ווב3</h1>
-  {#if $globalState.ownedToken === undefined || $globalState.ownedToken===0}
+  {#if $globalState.ownedToken === undefined || $globalState.ownedToken === 0}
     <MintButton />
   {:else}
-  <div class="text-center">
-    <img src={$globalState.image} alt="NFT" class="mx-auto my-2 h-64"/>
-    <div class="text-gray-400 p-4">קנית כרטיס מספר {$globalState.ownedToken}</div>
-  </div>
+    <div class="text-center">
+      <img src={$globalState.image} alt="NFT" class="mx-auto my-2 h-64" />
+      <div class="text-gray-400 p-4">
+        קנית כרטיס מספר {$globalState.ownedToken}
+      </div>
+    </div>
   {/if}
   <Dashboard />
+  <div class="fixed bottom-0 p-2 m-auto text-center text-gray-400 text-xs italic">
+    Credits: Yotam Bar-On, ChagitR, Royo
+  </div>
 </body>
