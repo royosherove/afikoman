7ed485e539ead253fc989a1c410cf2b7836f5855
Roy <roy@osherove.com>
2022-04-17 09:53:19 +0300

    metatags

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,9 +1,38 @@
 <script>
   import "../app.css";
+  import { MetaTags } from 'svelte-meta-tags';
 </script>
 
 <head>
-  <title>Afikoman</title>
+  <MetaTags
+    title="Afikoman 2022"
+    titleTemplate="%s | Web3 Israel Community"
+    description="הגרלת האפיקומן של ווב3"
+    canonical="https://afikoman.vercel.app/"
+    openGraph={{
+      url: 'https://afikoman.vercel.app/',
+      title: 'Afikoman 2022 Israel web3',
+      description: 'הגרלת האפיקוצן של ווב3',
+      images: [
+        {
+          url: 'https://web3il-afikoman.s3.eu-central-1.amazonaws.com/unrevealed.gif',
+          width: 800,
+          height: 600,
+          alt: 'Afiokoman'
+        },
+      ],
+      site_name: 'Afiokman 2022'
+    }}
+    twitter={{
+      handle: '@handle',
+      site: '@site',
+      cardType: 'summary_large_image',
+      title: 'Afikoman 2022',
+      description: 'אפיקומן 2022',
+      image: 'https://web3il-afikoman.s3.eu-central-1.amazonaws.com/unrevealed.gif',
+      imageAlt: 'Afikoman'
+    }}
+  />
   <link rel="preconnect" href="https://fonts.googleapis.com" />
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
   <link
