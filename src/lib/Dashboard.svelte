220bd00eb3c951fee6aa70f5fe1ebd132276a6cb
Roy <roy@osherove.com>
2022-04-22 11:28:16 +0300

    show card number

--- src/lib/Dashboard.svelte
+++ src/lib/Dashboard.svelte
@@ -15,9 +15,14 @@
   <div
     class="bg-gray-300 text-black m-2 sm:w-1/3 p-4 font-bold sm:m-auto rounded-lg text-sm flex flex-col"
   >
-    <div>מספר מחפשי אפיקומן: {tickets}</div>
     <!-- svelte-ignore a11y-label-has-associated-control -->
-{#if hasWinner}
+{#if $globalState.winnerOwned !== -1}
+    <div>מספר מחפשי אפיקומן: {tickets}</div>
+{/if}
+{#if hasWinner && $globalState.winnerOwned === -1}
+    <div class="flex flex-col mt-4 ">מעלה פרטים... <div alt='winner'>{winner}</div></div>
+{/if}
+{#if hasWinner && $globalState.winnerOwned !== -1}
     <div class="flex flex-col mt-4 ">הכרטיס שניצח<div alt='winner card'>{$globalState.winnerOwned}</div></div>
     <div class="flex flex-col mt-4 ">הארנק שניצח<div alt='winner'>{winner}</div></div>
 {/if}
